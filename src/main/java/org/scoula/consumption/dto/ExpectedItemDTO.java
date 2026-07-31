1e0e636a728ee987982af41e3ea1734f27487cb7
catlover <yewonseoul@gmail.com>
2026-07-31 13:13:16 +0900

    feat(consumption): add, update, and delete expected spending

--- src/main/java/org/scoula/consumption/dto/ExpectedItemDTO.java
+++ src/main/java/org/scoula/consumption/dto/ExpectedItemDTO.java
@@ -1,41 +1,23 @@
 package org.scoula.consumption.dto;
 
-// 일별 예상 소비 1건
-public class ExpectedItemDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExpectedItemDTO { // 일별 예상 소비 1건
     private Long expectedNo;
+
     private String categoryName;
+
     private Long amount;
+
     private String merchant;
 
-    public Long getExpectedNo() {
-        return expectedNo;
-    }
+    private String memo;
 
-    public void setExpectedNo(Long expectedNo) {
-        this.expectedNo = expectedNo;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Long amount) {
-        this.amount = amount;
-    }
-
-    public String getMerchant() {
-        return merchant;
-    }
-
-    public void setMerchant(String merchant) {
-        this.merchant = merchant;
-    }
 }
