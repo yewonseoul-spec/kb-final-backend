0d8cad8facdcdf505698b685f1833fc1b9a15b11
jayvic <jhdream01@gmail.com>
2026-08-04 15:39:07 +0900

    feat(mypage): implement password change API

--- src/main/java/org/scoula/member/exception/PasswordMissmatchException.java
+++ src/main/java/org/scoula/member/exception/PasswordMissmatchException.java
@@ -2,6 +2,6 @@ package org.scoula.member.exception;
 
 public class PasswordMissmatchException extends RuntimeException {
     public PasswordMissmatchException() {
-        super("비밀번호가 일치하지 않습니다.");
+        super("현재 비밀번호가 올바르지 않습니다.");
     }
 }
