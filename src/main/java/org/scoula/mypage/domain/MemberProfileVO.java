c5b7f296ce7978733bbd540a0a5fe54a1f66154a
jayvic <jhdream01@gmail.com>
2026-08-20 02:18:17 +0900

    refactor(mypage): remove household size

--- src/main/java/org/scoula/mypage/domain/MemberProfileVO.java
+++ src/main/java/org/scoula/mypage/domain/MemberProfileVO.java
@@ -18,7 +18,6 @@ public class MemberProfileVO {
     private Integer income;         // 미입력 허용 → int 아닌 Integer
     private String employStatus;
     private String major;
-    private Integer householdSize;  // 미입력 허용 → Integer
     private String education;
     private String mrgSttsCd;
     private String profileImgPath;  // 이미지 업로드는 별도 작업, 지금은 항상 null
